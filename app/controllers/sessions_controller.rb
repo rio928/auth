1d846aa146cda3ddb7afdc0e5231831da8aba9af
rio928 <rionex928@yahoo.co.jp>
2022-02-27 03:10:40 +0000

    changed

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+    def new
+    end
+
+    def create
+        entered_email = params["email"]
+        entered_password = params["password"]
+        @user = User.find_by({email: entered_email})
+
+        if @user
+            if BCrypt::Password.new(@user.password) == entered_password
+                flash[:notice] = "Welcome!"
+                redirect_to "/companies"
+            else
+                flash[:notice] = "Password is incorrect"
+                redirect_to "/sessions/new"
+            end
+        else 
+            flash[:notice] = "No user with that email address"
+            redirect_to "/sessions/new"
+        end
+    end
+end
